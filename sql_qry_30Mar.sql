0398b91e732f851b58f9c573d750d6398438e357
Code Bazinga <66390680+CodeBazinga@users.noreply.github.com>
2022-03-30 10:14:47 +0530

    Add files via upload

---
+++ sql_qry_30Mar.sql
@@ -0,0 +1,37 @@
+
+create table Transact
+(TID int primary key,
+TMode varchar(10) not null,
+Amount int not null,
+TSuccess bool
+-- Redeem_Rearwds bool
+)
+
+create table Flight_details(
+FNo varchar(10) primary key,
+Pil_Name varchar(30) not null,
+JDate date not null,
+Depart_time time not null,
+Arr_Time time not null,
+Source_City varchar(30) not null,
+Dest_City varchar(30) not null,
+Crew_members int
+)
+
+
+create table Pass_Details(
+PID int primary key,
+PName varchar(30) not null,
+Booking_Date date not null,
+Tr_ID int not null,
+Age int,
+F_no varchar(10) not null,
+F_Rewards int not null default 0,
+foreign key (F_No) references flight_details(FNo),
+foreign key (Tr_ID) references Transact(TID)
+)
+
+-- F_No( 'AI-745' - 'AI-750','IN-545','IN-549','SJ-324','SJ-320')
+-- Tr_ID(78965-78981)
+-- Age (18-65)
+
